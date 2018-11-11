1d0759cd87c459168e5d7fecb6d27d26325c1416
SeaRise <846579815@qq.com>
2018-11-12 06:42:49 +0800

    update DataBuffer

--- src/main/java/com/util/offheap/DataBuffer.java
+++ src/main/java/com/util/offheap/DataBuffer.java
@@ -3,12 +3,28 @@ package com.util.offheap;
 
 /*
  * thread unsafe
+ * 多线程使用必须加synchronized
+ * 
  * 可以修饰ByteBuffer,提供基本类型和String读写的功能
  * 实现方式参考了DataInputStream和DataOutputStream
+ * 
+ * 发现让stringBuffer和buffer合一的时候,string的读写就会出问题
+ * 找到原因了,string写的时候会写入length
+ * b.length > stringBuffer.length的时候,stringBuffer = b (b = string.getBytes)
+ * 如果stringBuffer和buffer合一,write length的时候会污染b
  * */
 public class DataBuffer implements ByteBuffer, DataOperator {
 
 	private final ByteBuffer byteBuffer;
+	
+	/**
+	 * 读写位置参考值,
+	 * 每次read后readPosition = position+dataOffset
+	 * 每次write后writePosition = position+dataOffset
+	 * 方便顺序读取和顺序写入,可以不用
+	 * */
+	private int readPosition = 0;
+	private int writePosition = 0;
 	
 	public DataBuffer(ByteBuffer byteBuffer) {
 		this.byteBuffer = byteBuffer;
@@ -41,28 +57,33 @@ public class DataBuffer implements ByteBuffer, DataOperator {
 	//byte
 	public final byte readByte(int position) {
 		byteBuffer.get(position, buffer, 0, 1);
+		readPosition = position+1;
         return buffer[0];
     }
 	
 	public final void writeByte(int position, byte value) {
 		buffer[0] = value;
 		byteBuffer.put(position, buffer, 0, 1);
+		writePosition = position+1;
     }
 	
 	//boolean
 	public final boolean readBoolean(int position) {
 		byteBuffer.get(position, buffer, 0, 1);
+		readPosition = position+1;
         return (buffer[0] != 0);
     }
 	
 	public final void writeBoolean(int position, boolean value) {
 		buffer[0] = (byte) (value ? 1 : 0);
 		byteBuffer.put(position, buffer, 0, 1);
+		writePosition = position+1;
     }
 	
 	//short
 	public final short readShort(int position) {
 		byteBuffer.get(position, buffer, 0, 2);
+		readPosition = position+2;
         return (short)((buffer[0] << 8) + (buffer[1] << 0));
     }
 	
@@ -70,11 +91,13 @@ public class DataBuffer implements ByteBuffer, DataOperator {
 		buffer[0] = (byte) ((value >>> 8) & 0xFF);
 		buffer[1] = (byte) ((value >>> 0) & 0xFF);
         byteBuffer.put(position, buffer, 0, 2);
+        writePosition = position+2;
     }
 	
 	//char
 	public final char readChar(int position) {
 		byteBuffer.get(position, buffer, 0, 2);
+		readPosition = position+2;
         return (char)((buffer[0] << 8) + (buffer[1] << 0));
     }
 	
@@ -82,11 +105,13 @@ public class DataBuffer implements ByteBuffer, DataOperator {
 		buffer[0] = (byte) ((value >>> 8) & 0xFF);
 		buffer[1] = (byte) ((value >>> 0) & 0xFF);
         byteBuffer.put(position, buffer, 0, 2);
+        writePosition = position+2;
     }
 	
 	//int
 	public final int readInt(int position) {
 		byteBuffer.get(position, buffer, 0, 4);
+		readPosition = position+4;
         return ((buffer[0] << 24) + (buffer[1] << 16) + (buffer[2] << 8) + (buffer[3] << 0));
     }
 	
@@ -96,11 +121,13 @@ public class DataBuffer implements ByteBuffer, DataOperator {
 		buffer[2] = (byte) ((value >>> 8) & 0xFF);
 		buffer[3] = (byte) ((value >>> 0) & 0xFF);
         byteBuffer.put(position, buffer, 0, 4);
+        writePosition = position+4;
     }
 	
 	//long
 	public final long readLong(int position) {
 		byteBuffer.get(position, buffer, 0, 8);
+		readPosition = position+8;
         return (((long)buffer[0] << 56) +
                 ((long)(buffer[1] & 255) << 48) +
                 ((long)(buffer[2] & 255) << 40) +
@@ -121,6 +148,7 @@ public class DataBuffer implements ByteBuffer, DataOperator {
 		buffer[6] = (byte) ((value >>> 8) & 0xFF);
 		buffer[7] = (byte) ((value >>> 0) & 0xFF);
         byteBuffer.put(position, buffer, 0, 8);
+        writePosition = position+8;
     }
 	
 	// float
@@ -146,29 +174,36 @@ public class DataBuffer implements ByteBuffer, DataOperator {
     }
 	
 	//string专用buffer
-	byte[] stringBuffer = new byte[8];
+	byte[] stringBuffer = new byte[0];
 	
 	//string
 	public final String readString(int position) {
 		int length = readInt(position);
-		//读byte数组不够大,扩容,扩容1.5倍防止反复扩容发生
+		//stringBuffer不够大,扩容,扩容1.5倍防止反复扩容发生
 		//不过扩容应该不会发生,因为writeString过程已经把stringBuffer替换成大的数组
-		if (stringBuffer.length < length) {
-			stringBuffer = new byte[length + (length >> 1)];
-		}
+		stringBuffer = stringBuffer.length < length ? 
+				new byte[length + (length >> 1)] : stringBuffer;
 		
 		get(position+4, stringBuffer, 0, length);
+		readPosition = position+4+length;
         return new String(stringBuffer, 0, length);
     }
 	
 	public final void writeString(int position, String value) {
 		byte[] b = value.getBytes();
-		//读byte数组没b大,就干脆用b,反正b也是要回收的,废物利用.
-		if (stringBuffer.length < readInt(position)) {
-			stringBuffer = b;
-		}
+		//stringBuffer没b大,就干脆用b,反正b也是要回收的,废物利用.
+		stringBuffer = stringBuffer.length < b.length ? b : stringBuffer;
 		
 		writeInt(position, b.length);
 		put(position+4, b, 0, b.length);
+		writePosition = position+4+b.length;
     }
+	
+	public int getReadPosition() {
+		return readPosition;
+	}
+
+	public int getWritePosition() {
+		return writePosition;
+	}
 }
--- src/test/java/com/util/offheap/TestDataBuffer.java
+++ src/test/java/com/util/offheap/TestDataBuffer.java
@@ -10,51 +10,46 @@ public class TestDataBuffer {
 	@Test
 	public void test() {
 		DataBuffer db = new DataBuffer(new DirectByteBuffer(100));
-		int position = 0;
-		
+	
 		//byte
-		db.writeByte(position, (byte) 0);
-		Assert.assertTrue(db.readByte(position) == (byte) 0);
-		position++;
+		db.writeByte(db.getWritePosition(), (byte) 0);
+		Assert.assertTrue(db.readByte(db.getReadPosition()) == (byte) 0);
 		
 		//boolean
-		db.writeBoolean(position, true);
-		Assert.assertTrue(db.readBoolean(position));
-		position++;
+		db.writeBoolean(db.getWritePosition(), true);
+		Assert.assertTrue(db.readBoolean(db.getReadPosition()));
 		
 		//short
-		db.writeShort(position, (short) 15);
-		Assert.assertTrue(db.readShort(position) == (short) 15);
-		position += 2;
+		db.writeShort(db.getWritePosition(), (short) 15);
+		Assert.assertTrue(db.readShort(db.getReadPosition()) == (short) 15);
 		
 		//int
-		db.writeInt(position, 16);
-		Assert.assertTrue(db.readInt(position) ==  16);
-		position += 4;		
+		db.writeInt(db.getWritePosition(), 16);
+		Assert.assertTrue(db.readInt(db.getReadPosition()) ==  16);	
 		
 		//long
-		db.writeLong(position, 17);
-		Assert.assertTrue(db.readLong(position) ==  17);
-		position += 8;
+		db.writeLong(db.getWritePosition(), 17);
+		Assert.assertTrue(db.readLong(db.getReadPosition()) ==  17);
 		
 		//float
 		//由于精度问题,读写很可能会有不一致问题,不推荐使用float
 		//这个是实现有问题
 		//不知道为啥f1和f2相差四倍
 		float f = (float) 5.0;
-		db.writeFloat(position, f);
-		System.out.println("f1: " + f);
-		System.out.println("f2: " + db.readFloat(position));
-		Assert.assertTrue(db.readFloat(position)*4 == f);
-		position += 4;
+		db.writeFloat(db.getWritePosition(), f);
+		//System.out.println("f1: " + f);
+		//System.out.println("f2: " + db.readFloat(position));
+		Assert.assertTrue(db.readFloat(db.getReadPosition())*4 == f);
 		
 		//double
-		db.writeDouble(position, 0.1);
-		Assert.assertTrue(db.readDouble(position) ==  0.1);
-		position += 8;
+		db.writeDouble(db.getWritePosition(), 0.1);
+		Assert.assertTrue(db.readDouble(db.getReadPosition()) ==  0.1);
 		
 		//string
-		db.writeString(position, "test test");
-		Assert.assertTrue(db.readString(position).equals("test test"));
+		db.writeString(db.getWritePosition(), "test test1");
+		Assert.assertTrue(db.readString(db.getReadPosition()).equals("test test1"));
+		
+		db.writeString(db.getWritePosition(), "test test2");
+		Assert.assertTrue(db.readString(db.getReadPosition()).equals("test test2"));
 	}
 }
