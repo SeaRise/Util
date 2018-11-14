8b7086ed4118174d8097171061997c8d74dcdd4b
SeaRise <846579815@qq.com>
2018-11-14 09:22:07 +0800

    update

--- src/main/java/com/util/cache/CacherOptions.java
+++ src/main/java/com/util/cache/CacherOptions.java
@@ -3,6 +3,7 @@ package com.util.cache;
 public interface CacherOptions<K, V> {
 	// 当key不在缓存中时, 则调用该函数取得对应资源.
 	// 该函数必须要是并发安全的.
+	// 不会同时有两个一样的key调用
 	V get(K key);
 	
 	// 释放资源的行为.
--- src/main/java/com/util/cache/RefCountCacher.java
+++ src/main/java/com/util/cache/RefCountCacher.java
@@ -4,11 +4,19 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /*
  * 一个引用计数法的缓存框架.
+ * 
+ * 想尽可能去做无锁,但是发现代码逻辑上有问题
+ * DRefCountCacher是安全的,不过加锁的地方很多.
  * */
 public class RefCountCacher<K, V> implements Cacher<K, V> {
+	
+	// Logger
+	private final static Logger LOGGER = Logger.getLogger(RefCountCacher.class.getName());
 	
 	private final ConcurrentHashMap<K, RefHolder<V>> refs = 
 			new ConcurrentHashMap<K, RefHolder<V>>();
@@ -39,6 +47,9 @@ public class RefCountCacher<K, V> implements Cacher<K, V> {
 			if (null != ref && ref.isGetting) {
 				//资源正在被其他线程获取
 				//这里不用两次判断,反正判错了也是continue
+				//判断错误的可能是状态变化到了
+				// - ref != null && !ref.isGetting(end get)
+				// - null == ref
 				doWhenGetting();
 				continue;
 			} else if (null != ref) {
@@ -49,11 +60,15 @@ public class RefCountCacher<K, V> implements Cacher<K, V> {
 					lock.lock();
 					ref = refs.get(key);
 					//两次判断
-					//这个条件在第二次判断会不成立的原因只会是realease了.ref == null
-					if (null != ref) {
+					//这个条件在第二次判断会不成立的原因可能是
+					// - realease了.ref == null
+					// - 其他线程正在getting,null != ref && ref.isGetting
+					if (null != ref && !ref.isGetting) {
 						ref.refCount.incrementAndGet();
+						LOGGER.log(Level.INFO, "return: " + key + "  count: " + ref.refCount);
 						return ref.value;
 					}
+					//ref已经被release了或这正在getting
 					//continue,下次循环get
 					continue;
 				} finally {
@@ -61,11 +76,11 @@ public class RefCountCacher<K, V> implements Cacher<K, V> {
 				}
 			} else {
 				//资源不在缓存中,也没有线程正在获取资源
-				final int maxHandles = options.getMaxHandles();
 				try {
 					lock.lock();
 					//缓存资源数目已达上限.
-					if (maxHandles > 0 && count >= maxHandles) {
+					if (options.getMaxHandles() > 0 && 
+							count >= options.getMaxHandles()) {
 						return null;
 					}
 					ref = refs.get(key);
@@ -78,12 +93,15 @@ public class RefCountCacher<K, V> implements Cacher<K, V> {
 					count++;
 					ref = new RefHolder<V>(true);
 					refs.put(key, ref);
+					LOGGER.log(Level.INFO, "getting: " + key);
 				} finally {
 					lock.unlock();
 				}
 				
 				//options.get本身应该是线程安全的.这里并发执行是为了防止lock加锁粒度太大
-				//能走到这一步的只可能是一个线程
+				//不可能会有多个对相同key操作的线程走到这里
+				//一个key只会有一个线程走到这里
+				//options.get的线程安全要求只要求不同key的操作线程安全就行
 				V cache = options.get(key);
 				if (null == cache) {
 					throw new RuntimeException("options get cache happen error");
@@ -91,6 +109,7 @@ public class RefCountCacher<K, V> implements Cacher<K, V> {
 				ref.value = cache;
 				ref.refCount = new AtomicInteger(1);
 				ref.isGetting = false;
+				LOGGER.log(Level.INFO, "had getten: " + key + "  count: " + ref.refCount);
 				return cache;
 			}
 		}
@@ -104,24 +123,34 @@ public class RefCountCacher<K, V> implements Cacher<K, V> {
 		}
 	}
 
+	//根据引用计数法,应该不会重复release才对.
+	//所以ref != null一定成立
+	//但是测试结果发现了有ref == null
 	public void release(K key) {
-		RefHolder<V> ref = refs.get(key);
 		//这里只判断refCount == 0是因为refCount不可能 < 0
-		if (0 == ref.refCount.decrementAndGet()) {
+		int c = refs.get(key).refCount.decrementAndGet();
+		if (0 == c) {
 			try {
 				lock.lock();
-				//两次判断
-				if (ref.refCount.get() > 0) {
+				LOGGER.log(Level.INFO, "begin realease: " + key + "  count: " + c);
+				//两次判断,在加锁之前,有线程又引用了key,那么引用数就>0
+				if (null == refs.get(key)) {
+					throw new RuntimeException("ref null key : " + key);
+				}
+				if (refs.get(key).refCount.get() > 0) {
 					return;
 				}
 				//release可以不是线程安全的.
-				options.release(key);
 				refs.remove(key);
 				count--;
+				options.release(key);
+				LOGGER.log(Level.INFO, "realease: " + key);
+				return;
 			} finally {
 				lock.unlock();
 			}
 		}
+		LOGGER.log(Level.INFO, "not get: " + key + "  count: " + c);
 	}
 
 	public void close() {
